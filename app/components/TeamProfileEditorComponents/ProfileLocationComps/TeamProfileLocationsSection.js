f6ae49a448c2d69105f0dcf22b362800b0269788
Mahmoud Al-Madi <mahmoud@textcopilot.com>
2024-12-18 18:38:41 -0600

    frontend updates - minor

--- app/components/TeamProfileEditorComponents/ProfileLocationComps/TeamProfileLocationsSection.js
+++ app/components/TeamProfileEditorComponents/ProfileLocationComps/TeamProfileLocationsSection.js
@@ -3,7 +3,17 @@ import InfoDropdown from "../InfoDropdown";
 export default function TeamProfileLocationsSection({ locationsInfo, parsedAddresses }) {
     return (
       <div className="w-full">
+        <div className="flex justify-between items-center mb-[16px]">
         <div className="text-[18px] font-bold text-streamlineBlue w-full">Team Locations</div>
+        <div className="flex text-[13px] font-bold text-white bg-green-500 px-[10px] py-[6px] rounded-full whitespace-nowrap space-x-[5px] items-center">
+          <div className="text-[16px]">
+          +
+          </div>
+          <div>
+          Add another location
+          </div>
+        </div>
+        </div>
         {/* <FaEllipsisV size={13} strokeWidth={0.5} /> */}
         {/* Parent Container */}
         <div className="w-full flex flex-col md:grid md:grid-cols-2 md:gap-4 py-[10px]">
